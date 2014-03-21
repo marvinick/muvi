efac7a7073176a408af115f5e4d77b5863117bc9
marvinick <marviluw@gmail.com>
2014-03-21 07:53:05 -0500

    test video and category models

---
+++ spec/models/video_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Video do
+  it "saves itself" do
+    video = Video.new(title: "monk", description: "nice!!")
+    video.save
+    expect(Video.first).to eq(video)
+  end
+
+  it "belongs to category" do
+    dramas = Category.create(name: "dramas")
+    monk = Video.create(title: "monk", description: "a great video!", category: dramas)
+    expect(monk.category).to eq(dramas)
+  end
+end
