5bf33d68ad8ff6f2502e3e0c65d12301dab453f2
marvinick <marviluw@gmail.com>
2014-03-22 13:28:16 -0500

    recent videos and test

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,171 @@ Video.create(title: "South Park", description: "Fourth grade political commentar
              small_cover_url: "/tmp/south_park.jpg",
              large_cover_url: "/tmp/south_park_large.jpg",
               category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 3)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 3)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+             category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 3)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 3)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 3)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 3)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 1)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 1)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 3)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 1)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 1)
+Video.create(title: "Futurama", description: "Philip J Fry blah blah",
+             small_cover_url: "/tmp/futurama.jpg",
+             large_cover_url: "/tmp/futurama_large.jpg",
+             category_id: 1)
+Video.create(title: "Family Guy", description: "Seth McFarlane blah blah Canada sucks",
+             small_cover_url: "/tmp/family_guy.jpg",
+             large_cover_url: "/tmp/family_guy_large.jpg")
+Video.create(title: "Monk", description: "OCD detective shenanigans",
+             small_cover_url: "/tmp/monk.jpg",
+             large_cover_url: "/tmp/monk_large.jpg",
+             category_id: 1)
+Video.create(title: "South Park", description: "Fourth grade political commentary",
+             small_cover_url: "/tmp/south_park.jpg",
+             large_cover_url: "/tmp/south_park_large.jpg",
+              category_id: 2)
 
 Category.create(name: "TV Comedies")
 Category.create(name: "TV Dramas")
