883a550893e0e50c659634ead73827473c5151f3
marvinick <marviluw@gmail.com>
2014-03-31 10:15:50 -0500

    user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :require_user, only: [:show]
   def new
     @user = User.new
   end
@@ -13,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
