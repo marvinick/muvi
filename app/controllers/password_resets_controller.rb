f5eace03b4dc2ac617c45d19346dc9e2367b434e
marvinick <marviluw@gmail.com>
2014-04-03 08:52:54 -0500

    resets password and tests

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,6 +9,16 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
+    user = User.where(token: params[:token]).first
+    if user
+      user.password = params[:password]
+      user.generate_token
+      user.save
+      flash[:success] = "You can sign in now, your password has been updated"
+      redirect_to sign_in_path
+    else
+      redirect_to expired_token_path
+    end
   end
 
   def expired_token
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -23,5 +23,40 @@ describe PasswordResetsController do
   end
 
   describe "POST create" do
+    context "with valid token" do
+      it "redirects to the sign in path" do
+        daddy = Fabricate(:user, password: 'old_password')
+        daddy.update_column(:token, '12345')
+        post :create, token: '12345', password: 'new_password'
+        expect(response).to redirect_to sign_in_path
+      end
+
+      it "updates the user's password" do
+        daddy = Fabricate(:user, password: 'old_password')
+        daddy.update_column(:token, '12345')
+        post :create, token: '12345', password: 'new_password'
+        expect(daddy.reload.authenticate('new_password')).to be_true
+      end
+
+      it "sets the flash success message" do
+        daddy = Fabricate(:user, password: 'old_password')
+        daddy.update_column(:token, '12345')
+        post :create, token: '12345', password: 'new_password'
+        expect(flash[:success]).to be_present
+      end
+
+      it "regenrates the user token" do
+        daddy = Fabricate(:user, password: 'old_password')
+        daddy.update_column(:token, '12345')
+        post :create, token: '12345', password: 'new_password'
+        expect(daddy.reload.token).not_to eq('12345')
+      end
+    end
+    context "with invalid token" do
+      it "redirects to the expired token path" do
+        post :create, token: '12345', password: 'some_password'
+        expect(response).to redirect_to expired_token_path
+      end
+    end
   end
 end
