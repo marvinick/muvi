3d3ea849e42ced6570092cc1b9ac0c5f3ce1877f
marvinick <marviluw@gmail.com>
2014-04-07 15:00:16 -0500

    admin adds video

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,6 +6,17 @@ class Admin::VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.create(video_params)
+    if @video.save
+      flash[:success] = "You have added a video '#{@video.title}'."
+      redirect_to new_admin_video_path
+    else
+      flash[:error] = "You cannot add this video. Please fix the issue first"
+      render :new
+    end
+  end
+
   private
 
   def require_admin
@@ -13,5 +24,9 @@ class Admin::VideosController < ApplicationController
       flash[:error] = "You are not authorized to do that"
       redirect_to home_path
     end
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :category_id, :description, :small_cover, :large_cover, :video_url)
   end
 end
