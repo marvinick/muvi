d3d5428390d245405e6a2fb514c09359694ed9a4
marvinick <marviluw@gmail.com>
2014-04-07 20:19:53 -0500

    user watch video

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407192745) do
+ActiveRecord::Schema.define(version: 20140408010332) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20140407192745) do
     t.integer  "category_id"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "video_url"
   end
 
 end
